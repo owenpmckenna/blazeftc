4da8b08b7bd70a9facea5e837982179846d55f1c
owen <owenpmckenna@outlook.com>
2026-05-26 10:58:27 -0400

    jni stack overflow fix probably

--- blazeftc/build.gradle.kts
+++ blazeftc/build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
         create<MavenPublication>("mavenLibrary") {
             groupId = "dev.anygeneric"
             artifactId = "blazeftc"
-            version = "0.1.28"
+            version = "0.1.29"
             description = "https://github.com/owenpmckenna/blaze_ftc"
 
 
