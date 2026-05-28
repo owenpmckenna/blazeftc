d6dad09cd7b74f8fcb38c15b563282a9db54052b
owen <owenpmckenna@outlook.com>
2026-05-28 17:33:09 -0400

    something... don't remember

--- blazeftc/build.gradle.kts
+++ blazeftc/build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
         create<MavenPublication>("mavenLibrary") {
             groupId = "dev.anygeneric"
             artifactId = "blazeftc"
-            version = "0.1.35"
+            version = "0.1.36"
             description = "https://github.com/owenpmckenna/blaze_ftc"
 
 
