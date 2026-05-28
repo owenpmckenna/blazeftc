932cc6c87dea87400ff5ed4e89c753635b385463
owen <owenpmckenna@outlook.com>
2026-05-27 22:29:50 -0400

    backup protection, 1010 micro sleep

--- blazeftc/build.gradle.kts
+++ blazeftc/build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
         create<MavenPublication>("mavenLibrary") {
             groupId = "dev.anygeneric"
             artifactId = "blazeftc"
-            version = "0.1.32"
+            version = "0.1.33"
             description = "https://github.com/owenpmckenna/blaze_ftc"
 
 
